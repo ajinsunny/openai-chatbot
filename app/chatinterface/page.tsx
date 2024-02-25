2eced5332a50d09de1af962061e83989a63ae9b1
Ajin Sunny <ajin.sunny@gmail.com>
2024-02-25 17:35:04 +0530

    Add ChatLayout component and dependencies

--- app/chatinterface/page.tsx
+++ app/chatinterface/page.tsx
@@ -1,5 +1,14 @@
 "use-client";
 
+import { ChatLayout } from "@/components/chat/chat-layout";
+import { cookies } from "next/headers";
+
 export default function ChatInterface() {
-  return <div>page</div>;
+  const layout = cookies().get("react-resizable-panels:layout");
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  return (
+    <div className="z-10 border rounded-lg max-w-5xl w-full h-full text-sm lg:flex">
+      <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
+    </div>
+  );
 }
